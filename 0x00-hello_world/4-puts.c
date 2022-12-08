6c294da0cd3f9755f91505e6ae3a884cc3b98c84
Chimmuanya <chimmuanyamogbo@gmail.com>
2022-12-08 14:33:27 -0800

    Update readme and answer question 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main -program entry point.
+ * prints a specific string to stdout using puts.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char st[] = "Programming is like building a multilingual puzzle";
+
+	puts(st);
+	return (0);
+}
