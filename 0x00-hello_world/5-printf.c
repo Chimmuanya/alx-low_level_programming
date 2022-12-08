042291a00c83ac0b32b39bdc1ce6b3bcfe11f5fa
Chimmuanya <chimmuanyamogbo@gmail.com>
2022-12-08 14:43:48 -0800

    Update readme and correct answer to question 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main -program entry point.
+ * prints a specific string to stdout using puts.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char st[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s",st);
+	return (0);
+}
