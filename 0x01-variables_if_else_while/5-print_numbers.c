aa2acd6c363bb7e8176aaaf2a51150e7f57c038d
Chimmuanya <chimmuanyamogbo@gmail.com>
2022-12-09 20:54:28 -0800

    Edit readme and answer question 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main -program entry point
+ * prints 0-9 without a newlint to stdout
+ * Return: zero (success)
+ */
+int main(void)
+{
+	int n;
+
+	while (n < 10)
+	{
+		putchar(48 + n);
+	}
+	putchar('\n');
+	return (0);
+}
