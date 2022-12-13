7dcfa6c18d62a93f3e8236f1d7db2586e37c9e5b
Chimmuanya <chimmuanyamogbo@gmail.com>
2022-12-13 15:31:43 -0800

    Update header and Readme, and answer question 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * print_islower - entry function
+ * check if char int is lowercase or not.
+ * @c: char to be checked.
+ * Return: int 1 if success or 0 otherwise
+ */
+int _islower(int c);
+{
+	int first = 97;
+	int last = 97 + 25;
+
+
+	if (c < first && c > last)
+	{
+		return (0);
+	}
+
+	return (1);
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,4 +3,5 @@ These projects will make use of header file main.h andc function _putchar.
 
 0. 0-putchar: prints _putchar to stdout
 1. 1-alphabet: prints lowercase alphabets to stdout using 'while'
-2. 2-print_alphabet_x10.c: prints lowercase alphabets 10 times 
+2. 2-print_alphabet_x10.c: prints lowercase alphabets 10 times.
+3. int _islower(int c): checks if int char is lowercase.
