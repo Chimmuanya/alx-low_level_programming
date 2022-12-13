39ebc5435ab7838ff372ee80fb3cbff07065bcdd
Chimmuanya <chimmuanyamogbo@gmail.com>
2022-12-13 15:05:10 -0800

    Update header and Readme, and answer question 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * print_alphabet - function to print alphabet using _putchar
+ * prints lowercase alphabets to stdout in 10 lines followed by a newline.
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i  = 0;
+
+	while (i < 10)
+	{
+		int start = 97;
+		int end = start + 25;
+
+		while (start <= end)
+		{
+			_putchar(start);
+			start++;
+		}
+
+		_putchar('\n';
+
+	}
+
+	_putchar('\n');
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,3 +3,4 @@ These projects will make use of header file main.h andc function _putchar.
 
 0. 0-putchar: prints _putchar to stdout
 1. 1-alphabet: prints lowercase alphabets to stdout using 'while'
+2. 2-print_alphabet_x10.c: prints lowercase alphabets 10 times 
