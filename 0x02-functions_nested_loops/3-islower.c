499bead720b675831fa8be5cb79ab8bb8bc41102
Chimmuanya <chimmuanyamogbo@gmail.com>
2022-12-13 15:45:58 -0800

    Edit answer to question 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /* betty style doc for function main goes there */
 /**
- * print_islower - entry function
+ * _islower - entry function
  * check if char int is lowercase or not.
  * @c: char to be checked.
  * Return: int 1 if success or 0 otherwise
@@ -12,8 +12,9 @@ int _islower(int c)
 	int first = 97;
 	int last = 97 + 25;
 
+	c = (int) c;
 
-	if (c < first && c > last)
+	if (c < first || c > last)
 	{
 		return (0);
 	}
