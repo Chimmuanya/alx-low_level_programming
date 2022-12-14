1f436b5283e54ca2c33d49d44247e8afc2fe394f
Chimmuanya <chimmuanyamogbo@gmail.com>
2022-12-14 10:24:24 -0800

    answer  question 12 -correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -44,18 +44,21 @@ void print_times_table(int n)
 			{
 				_putchar(COMMA);
 
-				if ((i >= 10) || (i * j >= 10) ||
-				    ((i * j < 10) && (i * (j + 1) >= 10)))
+				if ((i * j < 10) && (i * (j + 1) < 10))
+				{
 					_putchar(SPACE);
-				else if (((i * j < 100) && (i * (j + 1) >= 100))
-					 || (i * j >= 100))
-				{}
-				else if (i * j < 10)
+					_putchar(SPACE);
+					_putchar(SPACE);
+				}
+				else if (((i * j < 10) && (i * (j + 1) >= 10)) ||
+					i * j >= 10)
 				{
 					_putchar(SPACE);
 					_putchar(SPACE);
 				}
-				_putchar(SPACE);
+				else if (((i * j >= 10) && (i * (j + 1) >= 100))
+					 || i * j >= 100)
+				{}
 			}
 			j++;
 		}
