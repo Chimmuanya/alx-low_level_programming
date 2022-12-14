e3d4c0176cc7cfe6f285ea3fcf380a1f6c618b7a
Chimmuanya <chimmuanyamogbo@gmail.com>
2022-12-13 16:27:40 -0800

    Update header and Readme, and answer question 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * print_sign - entry function
+ * check if integer number is positive, negative or zero.
+ * @n: int to be checked.
+ * Return: int 1 if positive or 0 if zero or -1 if negative, print +, - or 0.
+ */
+int print_sign(int n)
+{
+	int plus = 43;
+	int minus = 45;
+	int zero = 48;
+
+
+	if (n > 0)
+	{
+		_putchar(plus);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(zero);
+		return (0);
+	}
+	else
+	{
+		putchar(minus);
+		return (-1);
+	}
+
+
+
+}
