028fb6fb9579d523c66449dfc4d48c90c5a8a681
Chimmuanya <chimmuanyamogbo@gmail.com>
2022-12-15 11:09:39 -0800

    update readme and main.h, answer question 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * _isdigit - entry function
+ * multiplies two integers.
+ * @a: integer to be multiplied.
+ * @b: other integer for multiplication
+ * Return: int product if success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 More Exercises on nested loops
 0. 0-isupper.c: returns 1 if a character is uppercase.
 2. 1-isdigit.c: returns 1 if character is a digit.
+3. 2-mul.c: multiplies 2 integers.
