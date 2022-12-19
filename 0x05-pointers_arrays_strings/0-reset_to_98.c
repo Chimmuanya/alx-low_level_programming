122df812ac35e7ba7e0b8b879c2ce31049c7ebe4
Chimmuanya <chimmuanyamogbo@gmail.com>
2022-12-19 14:50:37 -0800

    Create readme file, main.h files and answer question 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * reset_to_98- entry function
+ * changes variables values from pointers.
+ * @n: pointer varible for resetting calue.
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
