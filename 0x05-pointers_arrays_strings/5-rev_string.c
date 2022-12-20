548cc4784c7296bd8ff6d251545ddc0c6cf01510
Chimmuanya <chimmuanyamogbo@gmail.com>
2022-12-19 18:08:23 -0800

    update readme abd header, and answer question 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * print_rev - entry function
+ * reverse a string
+ * @s: string to be reversed.
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int str_len;
+	int end;
+	int index;
+	char start;
+
+	str_len = 0;
+
+	while (s[str_len] != 0)
+		str_len++;
+
+	str_len--;
+
+	for (index = 0; index <= (str_len / 2); index++)
+	{
+		start = s[index];
+		end = s[str_len - index];
+		s[index] = end;
+		s[str_len - index] = start;
+	}
+}
