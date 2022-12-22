04a0ed8eb205096b96a30aa7a7201e54ed221b59
Chimmuanya <chimmuanyamogbo@gmail.com>
2022-12-22 06:21:21 -0800

    answer q2

---
+++ 0x06-pointers_arrays_strings/.#2-strncpy.c
@@ -0,0 +1,1 @@
+root@3ebef7bd5f70.2845
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * _strncpy- entry function
+ * copies first n characters of a string to dest buffer.
+ * @src: string to be copied.
+ * @dest: destination buffer.
+ * @n: number of chars of src to be copied.
+ * Return:pointer to resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *dptr = dest;
+
+	for (; n != 0 && *src != '\0'; src++, n--)
+	{
+		*dptr = *src;
+		dptr++;
+	}
+
+	for (; n != 0 && *dptr != '\0'; n--)
+	{
+		*dptr = '\0';
+		dptr++;
+	}
+
+	return (dptr);
+}
