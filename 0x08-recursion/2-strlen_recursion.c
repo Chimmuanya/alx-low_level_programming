fcf36d95ff6cfd15b5cf31242c33d9d86bf164fb
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-01-04 12:42:19 -0800

    answer q2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * _strlen_recursion -entry function
+ * function that lenght of  a string.
+ * @s: string to be counted
+ * Return: int.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -7,3 +7,7 @@ Prototype: void _puts_recursion(char *s);
 2. Write a function that prints a string in reverse.
 
 Prototype: void _print_rev_recursion(char *s);
+
+3.  a function that returns the length of a string.
+
+Prototype: int _strlen_recursion(char *s);
