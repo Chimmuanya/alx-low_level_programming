161ead984dc8f0f8e3574a32d360b0d4d1a58ade
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-01-04 11:25:02 -0800

    correct faulty param

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*c == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(*c);
+	_putchar(*s);
 	c++;
-	_puts_recursion(c);
+	_puts_recursion(s);
 }
