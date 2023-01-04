3007ab9b6d67955afa73716c6a46069aaa70de05
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-01-04 12:12:19 -0800

    answer question 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * _print_rev_recursion -entry function
+ * function that prints a string in reverse.
+ * @s: string to be printed
+ * Return: void.
+ */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*C);
+		return;
+	}
+	else
+	{
+		_putchar('\n');
+		return;
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -4,3 +4,6 @@ Projects on recursion.
 
 Prototype: void _puts_recursion(char *s);
 
+2. Write a function that prints a string in reverse.
+
+Prototype: void _print_rev_recursion(char *s);
