f105ab1e63f9b26606c81345a983dc35f7e5f7be
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-01-06 12:45:41 -0800

    answer and edit q0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,24 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 
 /* betty style doc for function main goes there */
 /**
  * main -entry function
  * prints its name to stdout
- * @argc: -number of arguments passed to main.
+ * @argc: number of arguments.
  * @argv: -array of strings: arguments passed to main.
  * Return: 0 (success)
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	int i = argc;
+	char cwd[256];
+	/* char mod_fn; */
+
+	if (i)
+		getcwd(cwd, sizeof(cwd));
+
+	printf("%s%s\n", cwd, (argv[0] + 1));
 	return (0);
 }
