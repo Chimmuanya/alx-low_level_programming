49822d22e22efb624a890e699e857160df893bce
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-01-05 21:06:27 -0800

    edit q 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -29,7 +29,7 @@ int get_str_len(char* my_str)
 
 int pali_helper (char* my_str, int l, int r)
 {
-	if ('\0' == my_str)
+	if ('\0' == *my_str)
 		return (1);
 	else
 	{
@@ -42,7 +42,7 @@ int pali_helper (char* my_str, int l, int r)
 		if (*(my_str + l) == *(my_str + r))
 			return pali_helper(my_str, l + 1, r - 1);
 	}
-
+	return (0);
 }
 
 
