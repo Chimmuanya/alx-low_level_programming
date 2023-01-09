23fdff7cb4209a0813c168d0694ef34cb7b2089e
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-01-09 12:56:52 -0800

    add malloc return check

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int counter = 0;
 
 	if (size == 0)
-		return ('\0');
+		return (NULL);
 	str = malloc((sizeof(char)) * size);
 	ptr = str;
 
