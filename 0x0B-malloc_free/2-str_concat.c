e46fff776c6fb6b28c221f20f6cb72219234ec25
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-01-10 12:58:07 -0800

    reverse failed error handling

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -103,7 +103,5 @@ char *str_concat(char *s1, char *s2)
 	res = malloc(sizeof(char) * l + sizeof(char) * m + 1);
 	/* create concatenated str */
 	concat(s1, s2, res, l, m);
-	if (res == NULL)
-		return (NULL);
 	return (res);
 }
