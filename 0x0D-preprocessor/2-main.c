3f339a7f15f589835797c09d84d9df025ecfa74b
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-01-16 14:51:37 -0800

    answer q2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/* betty style doc for function main goes there */
+/**
+ * main -entry function
+ * prints the filename.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
