daed437582ab13548551cd67b592d195c114ca0e
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-01-16 15:33:47 -0800

    answer q0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+
+/**
+ * struct dog - Dog information
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's owner
+ *
+ * Description: for dog's Longer description. stores dog name, age
+ * and dog ownwer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
