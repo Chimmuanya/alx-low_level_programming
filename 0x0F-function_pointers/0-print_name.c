34e8aeb88a3782e72e0a7a7a11696fd477ea23d8
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-01-18 14:06:44 -0800

    adjust return statement and header file name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,10 +6,9 @@
  * print_name -function to call a function that prints a str
  * @f: pointer to callback function.
  * @name: char array to be printed.
- * Return: 0 (success)
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
-	return (0);
 }
