a7ea7237e1ddc1ff28fcfc65604d95c69edba78d
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-01-19 14:26:29 -0800

    correct betty linter indent error

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -30,7 +30,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", s);
 
 		if (i < n - 1)
-                {
+		{
 			if (separator)
 				printf("%s", separator);
 		}
