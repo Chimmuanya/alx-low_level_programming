eff4e768f03ca8de444707b5e0152000e0e8f008
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-01-26 16:12:00 -0800

    edit q6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,15 @@
 	SECTIOM .data
 msg:	db "Hello, Holberton\n", 0
-fmt:	db "%s", 15, 0
+fmt:	db "%s", 10, 0
 
 	SECTION .text
 	extern printf
-	global main, _start
+	global main
 main:
-_start:
 	mov esi, msg
 	mov edi fmt
 	mov eax, 0
 	call printf
 
-	mov ebx, 0
-	mov eax, 1
+	mov eax, 0
+	ret
