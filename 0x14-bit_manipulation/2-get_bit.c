ce67cf8ceb52167845e220c547d216977c1c00b7
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-02-03 13:30:18 -0800

    answer q2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * get_bit -returns the bit at position index of an integer
+ *
+ * @n: the long int to be converted and printed
+ * @index: position of bit of interest in binary notation
+ * Return: Returns bit integer
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+			return (n & 1);
+	}
+	return (-1);
+}
