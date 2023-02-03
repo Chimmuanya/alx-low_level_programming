3da8ee10a5b8d5b41a5f4b3719e0eccf755a577f
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-02-03 11:41:57 -0800

    correct erroneous conditional

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * binary_to_uint -entry function
+ * converts a binary number to an unsigned int.
+ * fills the first n bytes of the memory area pointed to by s with
+ * @b: the constant char holding the bin num
+ *
+ * Return: Returns the unsigned int
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int i = 0;
+	int len = 0;
+	unsigned int base_two = 1;
+
+	for (len = 0; *(b + len); len++)
+		;
+	for (len--; len >= 0; len--, base_two *= 2)
+	{
+
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		if (b[len] & 1)
+			i += base_two;
+	}
+	return (i);
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -3,7 +3,7 @@
 |File name | Description |
 |----------|-------------|
 | `0-binary_to_uint.c` | A Write a function that converts a binary number to an unsigned int.|
-| `1-listint_len.c` | A function that returns the number of elements in a linked listint_t list. |
+| `1-print_binary.c` | A function that prints the binary representation of a number.|
 | `2-add_nodeint.c` | A function that adds a new node at the beginning of a listint_t list. |
 | `3-add_nodeint_end.c` |  A function that adds a new node at the end of a listint_t list. |
 | `4-free_listint.c` | A function that frees a listint_t list. |
