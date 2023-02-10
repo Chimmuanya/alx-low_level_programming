a2157b6cfc4d85b8a36b2794793c7778a97176ca
Chimmuanya <chimmuanyamogbo@gmail.com>
2023-02-10 11:24:25 -0800

    answer q3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,101 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+
+char *make_buffer(char *);
+void close_f(int);
+
+/**
+  * main -entry function
+  * @argc: number of arguments
+  * @argv: array of pointers to argument array
+  * Return: intege
+  */
+int main(int argc, char **argv)
+{
+	char *file_from, *file_to, *buffer;
+	int from, to, r, w;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = argv[1];
+	file_to = argv[2];
+	buffer = make_buffer(file_to);
+
+	from = open(file_from, O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || to == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", file_to);
+			free(buffer);
+			exit(98);
+		}
+		w = write(to, buffer, r);
+		if (w == -1 || to == -1)
+		{
+			dprintf(STDERR_FILENO,
+				 "Error: Can't write to %s", file_to);
+			free(buffer);
+			exit(99);
+		}
+		r = read(from, buffer, 1024);
+		to = open(file_to, O_WRONLY | O_APPEND);
+	} while (r > 0);
+	free(buffer);
+	close_f(from);
+	close_f(to);
+	return (0);
+}
+
+/**
+ * make_buffer -function to create buffer of size with 1024 chars
+ * @file_to: filename of file to be written to from created buffer
+ * Return: pointer to buffer
+ */
+
+char *make_buffer(char *file_to)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (!buffer)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s", file_to);
+		exit(99);
+	}
+	return (buffer);
+}
+
+
+
+/**
+ * close_f -entry function
+ * closes an open file descriptor.
+ * @f_d: file descriptor
+ *
+ * Return: Returns void
+ */
+void close_f(int f_d)
+{
+	int i;
+
+	i = close(f_d);
+
+	if (i == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't close fd %i", f_d);
+		exit(100);
+	}
+}
+/* help from Isaac Samuel Birhanu's code acknowledged*/
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -4,9 +4,6 @@
 |----------|-------------|
 | `0-read_textfile.c` | A Write a function that reads a text file and prints it to the POSIX standard output..|
 | `1-create_file.c`.| a function that creates a file. |
-| `2-append_text_to_file.c` |Append to file. |
-| `3-set_bit.c` | set an integer's bit at positionn index. |
-| `4-clear_bit.c` |  a function that sets the value of a bit to 0 at a given index. |
-| `5-flip_bits.c` | A function that returns the number of bits you would need to flip to get from one number to another. |
-| `100-get_endianness.c` | A function that checks the endianness. |
+| `2-append_text_to_file.c` | Append to file. |
+| `3-cp.c | copy contents of one file to the other |
 | `101-password` | find the password for crackme |
